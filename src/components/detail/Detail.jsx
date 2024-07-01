e64ef5b275f40954c3c6d9bf3aa1bd304b122fa3
likeMike001 <like.mike.007.382@gmail.com>
2024-07-01 00:29:05 -0700

    added chat list and detail comp

---
+++ src/components/detail/Detail.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./detail.css"
+
+
+const Detail = () => {
+  return (
+    <div className="detail">
+        Detial 
+      
+    </div>
+  )
+}
+
+export default Detail
