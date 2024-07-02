773521ad72dacdca60de7973c1dc57d6ba61c50f
likeMike001 <like.mike.007.382@gmail.com>
2024-07-01 22:49:39 -0700

    added styles for scrolls and borders

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -3,21 +3,48 @@ import "./chatlist.css"
 
 const ChatList = () => {
 
-  const [addMode , setAddMode] = useState("false")
+  const [addMode, setAddMode] = useState("false")
 
   return (
     <div className="chatList">
       <div className="search">
         <div className="searchBar">
-          <img src="./search.png" alt=""  />
+          <img src="./search.png" alt="" />
           <input type="text" placeholder="Search"></input>
         </div>
-        <img src={addMode ? "./minus.png" : "./plus.png"} 
-        alt="" 
-        className="add" 
-        onClick={() => setAddMode((prev) => !prev)}
-        
+        <img src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
         />
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe </span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe </span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe </span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe </span>
+          <p>Hello</p>
+        </div>
       </div>
     </div>
   )
