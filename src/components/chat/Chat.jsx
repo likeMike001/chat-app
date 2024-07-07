898894b6bf1267e3b163f17a34a2449ebe1a7464
likeMike001 <like.mike.007.382@gmail.com>
2024-07-07 08:49:15 -0700

    added styles for the detial comp

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -5,17 +5,17 @@ import { useState } from "react"
 
 const Chat = () => {
 
-  const [open,setOpen] = useState(false)
-  const [text,setText] = useState("")
+  const [open, setOpen] = useState(false)
+  const [text, setText] = useState("")
 
   // console.log(text)
 
   const handlEmo = e => {
     // console.log(e)
 
-    setText((prev) => prev+e.emoji)
+    setText((prev) => prev + e.emoji)
     setOpen(false)
-  
+
   }
 
   return (
@@ -26,7 +26,7 @@ const Chat = () => {
         <div className="user">
           <img src="./avatar.png" alt="" />
           <div className="texts">
-            <span>Jane Doe</span>
+            <span>Rudresh Bhandari </span>
             <p>Loren Ipsum dolor , sit amef</p>
           </div>
         </div>
@@ -37,24 +37,66 @@ const Chat = () => {
         </div>
       </div>
       <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="avatar-img" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque repellendus assumenda consequuntur illo aspernatur. Cupiditate obcaecati omnis quae totam sed, ipsum quis! Temporibus adipisci, ea quisquam quae expedita itaque harum?</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque repellendus assumenda consequuntur illo aspernatur. Cupiditate obcaecati omnis quae totam sed, ipsum quis! Temporibus adipisci, ea quisquam quae expedita itaque harum?</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="avatar-img" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque repellendus assumenda consequuntur illo aspernatur. Cupiditate obcaecati omnis quae totam sed, ipsum quis! Temporibus adipisci, ea quisquam quae expedita itaque harum?</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque repellendus assumenda consequuntur illo aspernatur. Cupiditate obcaecati omnis quae totam sed, ipsum quis! Temporibus adipisci, ea quisquam quae expedita itaque harum?</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="avatar-img" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque repellendus assumenda consequuntur illo aspernatur. Cupiditate obcaecati omnis quae totam sed, ipsum quis! Temporibus adipisci, ea quisquam quae expedita itaque harum?</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="https://www.pexels.com/photo/close-photography-of-red-and-pink-rose-56866/" alt=" "  />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque repellendus assumenda consequuntur illo aspernatur. Cupiditate obcaecati omnis quae totam sed, ipsum quis! Temporibus adipisci, ea quisquam quae expedita itaque harum?</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
       </div>
       <div className="bottom">
         <div className="icons">
-          <img src="./img.png"  alt=''/>
-          <img src="./camera.png"  alt=''/>
-          <img src="./mic.png"  alt=''/>
+          <img src="./img.png" alt='' />
+          <img src="./camera.png" alt='' />
+          <img src="./mic.png" alt='' />
         </div>
-        <input type="text" placeholder="Write a message ...." 
+        <input type="text" placeholder="Write a message ...."
           value={text}
           onChange={(e) => setText(e.target.value)}
-        
+
         />
         <div className="emoji">
-          <img src="./emoji.png" 
-          alt="" 
-          onClick={() => setOpen((prev) => !prev )}
+          <img src="./emoji.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
           />
-          <EmojiPicker  open={open} onEmojiClick={handlEmo}/>
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handlEmo} />
+          </div>
         </div>
         <button className="sentButton">Send </button>
       </div>
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,11 +2,81 @@ import React from 'react'
 import "./detail.css"
 
 
+
 const Detail = () => {
   return (
     <div className="detail">
-        Detial 
-      
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Loren ispum dolor ist emnet.</p>
+      </div>
+      <div className='info' >
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src='./arrowUp.png' alt=" " />
+          </div>
+
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy and Help</span>
+            <img src='./arrowUp.png' alt=" " />
+          </div>
+
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy and Help</span>
+            <img src='./arrowUp.png' alt=" " />
+          </div>
+
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos </span>
+            <img src='./arrowDown.png' alt=" " />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://www.pexels.com/photo/brown-hummingbird-selective-focus-photography-1133957/" alt=""></img>
+                <span>photo_212</span>
+              </div>
+              <img src="./download.png" alt="" className="icon"></img>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://www.pexels.com/photo/brown-hummingbird-selective-focus-photography-1133957/" alt=""></img>
+                <span>photo_212</span>
+              </div>
+              <img src="./download.png" alt="" className="icon"></img>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://www.pexels.com/photo/brown-hummingbird-selective-focus-photography-1133957/" alt=""></img>
+                <span>photo_212</span>
+              </div>
+              <img src="./download.png" alt="" className="icon"></img>
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://www.pexels.com/photo/brown-hummingbird-selective-focus-photography-1133957/" alt=""></img>
+                <span>photo_212</span>
+              </div>
+              <img src="./download.png" alt="" className="icon"></img>
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span> Shared Files  </span>
+            <img src='./arrowUp.png' alt=" " />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
     </div>
   )
 }
